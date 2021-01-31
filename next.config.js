0a71a581c73f56255b7c128ce2b73401765060f3
Antonio Ivanovski <antonioivanovski@gmail.com>
2021-01-31 20:46:56 +0100

    Add redirect for old info.html

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const { PHASE_PRODUCTION_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
-  const isProductionServer = phase === PHASE_PRODUCTION_SERVER
   return {
     ...defaultConfig,
-    env: Object.assign(defaultConfig.env || {}, {
-      publicDirPath: isProductionServer ? '/' : process.cwd() + '/public'
-    }),
+    async redirects() {
+      return [
+        {
+          source: '/info.html',
+          destination:
+            'https://docs.google.com/document/d/e/2PACX-1vTBdvrqaIkgl8WD-W9T3vZ2yPyL48aQYIik1aXKJeZ3HIpPKyCEKwnTYhKJGeo0ylXzAEJPkccG0B-I/pub',
+          permanent: false
+        }
+      ]
+    },
     webpack: (config, { isServer }) => {
       // Fixes npm packages that depend on `fs` module
       if (!isServer) {
